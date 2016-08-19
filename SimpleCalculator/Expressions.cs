bbe2f1203f11652422c8441eff6cd18d4b70f59f
Sandy Jones <sandynashv@gmail.com>
2016-08-18 20:16:00 -0500

    mathematical operations' tests

--- SimpleCalculator/Expressions.cs
+++ SimpleCalculator/Expressions.cs
@@ -8,12 +8,53 @@ namespace SimpleCalculator
 {
     public class Expresssions
     {
+        
+        public string userResponse;
+        //*************WORKING ON THIS ******************
+        //possibly use String.Comparer 
+
+        public StringComparer OrdinalIgnoreCase {get;}
+        private string quit = "quit";
+        private string exit = "exit";
+        private string last = "last";
+        private string lastq = "lastq";
+
+        // public void Match(string userResponse)
+        //{
+        //    Match match = Expressions.Match(userResponse)
+        //    if (userResponse = "quit")
+        //     {
+        //         break;
+        //     } else if (userResponse = "exit")
+        //     {
+        //         break;
+        //     } else if (userResponse = "last")
+        //     {
+        //         Console.WriteLine("Last Printed Answer");
+        //     } else if (userResponse = "lastq")
+        //     {
+        //         Console.WriteLine("Last printed command or expression.")
+        //     }
+
+
+        //        Term1 = int.Parse(match.Groups[1].Value);
+        //        Term2 = int.Parse(match.Groups[3].Value);
+        //        Operator = match.Groups[2].Value[0];
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentException("Please enter a valid integer.");
+        //    }
+        //}
+
+
+
         int AddTwoNumbers(int number1, int number2)
       {
             return number1 + number2;
       }
       int SubtractTwoNumbers(int number1, int number2)
-     {
+      {
           return number1 - number2;
       }
       int MultiplyTwoNumbers(int number1, int number2)
